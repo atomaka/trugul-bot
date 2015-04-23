35d9a0e910d5b788106c7afd0f0f703d444a8dec
Andrew Tomaka <atomaka@gmail.com>
2015-04-23 14:46:51 -0400

    update the design a bit

--- app.rb
+++ app.rb
@@ -5,8 +5,9 @@ require './environments'
 
 set :public_folder, 'public'
 
+# CONTROLLER
 get '/' do
-  @raids = Raid.all
+  @raids = Raid.all.reverse
   erb :index
 end
 
@@ -24,6 +25,28 @@ post '/' do
   end
 end
 
+#HELPERS
+def readable_number(value)
+  numbers.each do |number, symbol|
+    if value.to_f / number.to_f > 1
+      return (value.to_f / number.to_f).to_s + symbol
+    end
+  end
+
+  return value
+end
+
+def numbers
+  {
+    '1000000000000000' => 'Q',
+    '1000000000000' => 'T',
+    '1000000000' => 'B',
+    '1000000' => 'M',
+    '1000' => 'K'
+  }
+end
+
+# MODEL
 class Raid < ActiveRecord::Base
   def money=(value)
     value = value.to_s.gsub(/[$,]/, '').to_f
