ae8c3a8b55258d346fe2b8ce22c8e7f0737d7eaf
Andrew Tomaka <atomaka@gmail.com>
2015-04-23 21:49:43 -0400

    revert

--- app.rb
+++ app.rb
@@ -70,7 +70,7 @@ def readable_number(value)
   negative = false
 
   #negative = true if value =~ /\-/
-  value.gsub!(/\-/, '')
+  #value.gsub!(/\-/, '')
   numbers.each do |number, symbol|
     if value.to_f / number.to_f > 1
       return sprintf('%.2f', value.to_f / number.to_f) + '<strong>' + symbol + '</strong>'
